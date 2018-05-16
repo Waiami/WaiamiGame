28640f43075597542b8034dc909ccd032d93d5ff
lordigquest <32837857+lordigquest@users.noreply.github.com>
2018-05-16 11:13:44 +0200

    explosion update

--- MiamiProject/Assets/Scripts/Weapon/Explosion.cs
+++ MiamiProject/Assets/Scripts/Weapon/Explosion.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Explosion : MonoBehaviour {
     [SerializeField] private Animator explosionAnimator;
     private PlayerDataModel referencePlayerDataModel;
-	// Use this for initialization
+    public LayerMask layerMask;
+	
+    // Use this for initialization
 	void Start () {
         if(explosionAnimator == null)
         {
             explosionAnimator = GetComponent<Animator>();
+            
         }
-	}
+        transform.localScale = new Vector3(4.9f, 4.9f , 1);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,6 +25,7 @@ public class Explosion : MonoBehaviour {
             Destroy(this.gameObject);
         }
 	}
+
     public void SetReferencePlayerDataModel(PlayerDataModel value)
     {
         referencePlayerDataModel = value;    
@@ -29,12 +35,28 @@ public class Explosion : MonoBehaviour {
     {
         if (collision.tag == "Player")
         {
-            if (referencePlayerDataModel != collision.gameObject.GetComponent<PlayerDataModel>())
+            if(!IsWallBetween(this.transform.position, collision.transform.position))
             {
-                referencePlayerDataModel.GetComponent<PlayerController>().AddPoints(collision.gameObject.GetComponent<PlayerDataModel>().PointsWorth);
+                if (referencePlayerDataModel != collision.gameObject.GetComponent<PlayerDataModel>())
+                {
+                    referencePlayerDataModel.GetComponent<PlayerController>().AddPoints(collision.gameObject.GetComponent<PlayerDataModel>().PointsWorth);
+                }
+                collision.gameObject.GetComponent<PlayerController>().KillPlayer();
             }
-            collision.gameObject.GetComponent<PlayerController>().KillPlayer();
         }
+    }
+
+    private bool IsWallBetween(Vector2 origin, Vector2 target)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(origin, (target - origin), (origin-target).magnitude, layerMask);
+        if(hit.collider != null)
+        {
+            if(hit.collider.tag == "Wall")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
